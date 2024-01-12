ff18d3c4d0eba4961d4e45d4b509af9aeac75210
Hubert Gruniaux <hubert@gruniaux.com>
2024-01-12 15:50:44 +0100

    Fix formatting and documentation.

--- memory.h
+++ memory.h
@@ -16,8 +16,10 @@ typedef uint32_t addr_t;
 extern "C" {
 #endif
 
-/** Checks if the given @a ptr is not NULL. If it is, dumps an error message and abort. */
-void check_alloc(void* ptr);
+/** Checks if the given @a ptr is not NULL.
+ *
+ * If it NULL, dumps an error message and abort. */
+void check_alloc(void *ptr);
 
 /*
  * RAM abstraction.
@@ -25,28 +27,32 @@ void check_alloc(void* ptr);
 
 typedef struct ram_t ram_t;
 
-/** Creates an infinite-size RAM block. */
+/** Creates an infinite size RAM block. */
 ram_t *ram_create();
-ram_t *ram_from_file(const char* filename);
+/** Creates an infinite size RAM block first initialized with the data stored at
+ * @a filename. */
+ram_t *ram_from_file(const char *filename);
 /** Destroys the given @a ram block. */
-void ram_destroy(ram_t* ram);
+void ram_destroy(ram_t *ram);
 /** Gets the word at the given @a addr of the given @a ram block. */
-word_t ram_get(ram_t* ram, addr_t addr);
+word_t ram_get(ram_t *ram, addr_t addr);
 /** Sets the word at the given @a addr to @a value of the given @a ram block. */
-void ram_set(ram_t* ram, addr_t addr, word_t value);
+void ram_set(ram_t *ram, addr_t addr, word_t value);
 /** Same as ram_get() then ram_set(), but faster. */
-word_t ram_get_set(ram_t* ram, addr_t addr, word_t value);
+word_t ram_get_set(ram_t *ram, addr_t addr, word_t value);
 
 /*
  * ROM abstraction.
  */
 
-typedef struct rom_t { const word_t* data; } rom_t;
+typedef struct rom_t {
+  const word_t *data;
+} rom_t;
 
 /** Creates a ROM block with the given initial @a data of length @a data_len. */
 rom_t rom_create(const word_t *data, size_t data_len);
 /** Creates a ROM block with the data stored in the file @a filename. */
-rom_t rom_from_file(const char* filename);
+rom_t rom_from_file(const char *filename);
 /** Destroys the given @a rom block. */
 void rom_destroy(rom_t rom);
 /** Gets the word at the given @a addr of the given @a rom block. */
